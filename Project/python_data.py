2ba18da06f7802c52d00f38328bbb9a83c43f981
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-12 15:13:47 +0300

    determine the average sale price of each product category

--- Project/python_data.py
+++ Project/python_data.py
@@ -1,4 +1,6 @@
 import pandas as p
+from matplotlib import pyplot as plt
+from pandas import DataFrame
 
 # Use Pandas to read in the sales data from the CSV file provided in the
 # “student materials” folder on Slack (sales_dataset.csv)
@@ -12,30 +14,26 @@ month = df['Month']
 customer_name = df["Customer Name"]
 sales = df['Sale Price']
 
+df["Total Sales"] = df["Quantity Sold"] * df["Sale Price"]
 
-# print(customer_name)
-
-# category_grouped = df.groupby('Product Name')
-# print(category_grouped)
-
-#
-# sales_by_product = df.groupby("Customer Name")["Sale Price"].max()
-# print(sales_by_product)
-#
 
 # # Calculate the total sales for each product
 def total_sales():
     # multiply the two columns quantity sold and sales price to get total sale of each product
-    df["Total Sales"] = df["Quantity Sold"] * df["Sale Price"]
-    print(df)
+    sales_by_product = df.groupby("Product Name")["Total Sales"].sum()
+    sales_by_product.to_csv('product_total_sales.csv')
+    # print(sales_by_product)
+    return sales_by_product
 
-total_sales()
-#
-# # Determine the average sale price for each product category
-# def average_sale_price():
-#     print()
-#
+
+
+# Determine the average sale price for each product category
+def average_sale_price():
+    # group each product by category
+    average_sale_categories = df.groupby("Category")['Total Sales'].mean()
+    # print(average_sale_categories)
+    average_sale_categories.to_csv('category_average_price.csv')
+    return average_sale_categories
+
+
 # # Identify the month with the highest sales and the month with the lowest sales
-#
-# # Determine which customers made the most purchases and how much they spent in total
-# # Write the results of your analysis as a CSV file
